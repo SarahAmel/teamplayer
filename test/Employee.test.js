1c52fd6447f5b38257fff92efbfa7c5859c33092
sarah attar <sarahattar@sarahs-MacBook-Pro.local>
2022-05-30 15:55:45 -0400

    added folder structure and started Employee test

---
+++ test/Employee.test.js
@@ -0,0 +1,52 @@
+// require the employee from the lib folder//
+
+const 
+
+
+
+
+
+//test can begin employee instance //
+
+
+
+
+
+// test can set a name via constructor args //
+
+
+
+
+// can set id via constructor args 
+
+
+
+
+
+
+// can set email via constructor args 
+
+
+
+
+
+
+// can set name via constructor function
+
+
+
+
+
+
+// can set id via constructor function 
+
+
+
+
+
+// can get email via constructor function
+
+
+
+
+// role function should return employee 
